114ff42171b7ba92b4c6911f8a8192f3decc5010
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 09:17:07 -0600

    Initialize class and add attributes to hash

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,2 +1,12 @@
 class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
+  def initialize(details)
+    @number = details[:number]
+    @monthly_rent = details[:monthly_rent]
+    @bathrooms = details[:bathrooms]
+    @bedrooms = details[:bedrooms]
+    @renter = renter
+  end
+
 end
