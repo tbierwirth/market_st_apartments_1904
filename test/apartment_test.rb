fd57bed6d0d1cdc1450e76745c331c8a21716987
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 09:16:16 -0600

    Add tests for details of apartment

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -5,11 +5,19 @@ require './lib/renter'
 
 class ApartmentTest < Minitest::Test
   def setup
-    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathroom: 1, bedrooms: 1})
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
   end
 
   def test_for_instance
     assert_instance_of Apartment, @a1
+  end
+
+  def test_details
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+    assert_nil @a1.renter
   end
 
 end
