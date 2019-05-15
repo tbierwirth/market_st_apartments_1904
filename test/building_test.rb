a8b7b7ce92713248148ae20077054aa88c240c35
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 09:52:13 -0600

    Add test for highest rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -10,6 +10,7 @@ class RenterTest < Minitest::Test
     @building = Building.new
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @spencer = Renter.new("Spencer")
   end
 
   def test_for_instance
@@ -18,15 +19,26 @@ class RenterTest < Minitest::Test
 
   def test_for_adding_units
     assert_equal [], @building.units
+    
     @building.add_unit(@a1)
     @building.add_unit(@b2)
+
     assert_equal [@a1, @b2], @building.units
   end
 
   def test_for_average_rent
     @building.add_unit(@a1)
     @building.add_unit(@b2)
+
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_for_highest_rent
+    @b2.add_renter(@spencer)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal @spencer, @building.renter_with_highest_rent
   end
 
 end
