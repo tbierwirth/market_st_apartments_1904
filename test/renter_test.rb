4b8e1872f521d7927fb448c78c7df6a93252a3db
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 09:07:13 -0600

    Create initial test setup and test for instance

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,4 +3,13 @@ require 'minitest/pride'
 require './lib/renter'
 
 class RenterTest < Minitest::Test
+
+  def setup
+  @jessie = Renter.new("Jessie")
+  end
+
+  def test_instance_of
+    assert_instance_of Renter, @jessie
+  end
+
 end
