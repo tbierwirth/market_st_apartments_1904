4e3cbdc99a59f3aded474bce903a9f46fdd5a54e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 09:12:32 -0600

    Create initial test setup and test for instance

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,12 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+  def setup
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathroom: 1, bedrooms: 1})
+  end
+
+  def test_for_instance
+    assert_instance_of Apartment, @a1
+  end
+
 end
